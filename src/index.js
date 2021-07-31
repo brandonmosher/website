8c7390f74461603b93c8ba186e991d7dfa772098
Brandon Mosher <code.brandon@mosher.xyz>
2021-07-30 20:43:03 -0400

    More style refinements for main page and components. Removed auto-fit font in favor of viewport based text scaling.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import "Component/on-intersection-add-class";
 import "Component/progress-bar";
 import "Component/tinted-background";
 import "Component/git";
-import "Component/autofit-font";
 import "Component/parallax";
 import "Component/nav";
 import "element-internals-polyfill";
