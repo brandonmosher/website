f883f2fe7beb6067a73863e4c157044b32e376c2
Brandon Mosher <code.brandon@mosher.xyz>
2021-08-04 17:08:06 -0400

    Scrollbar styling. Added resume link. Summary and vertical-timeline now automatically populated from resume JSON file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import "Font/avenir-35-light/avenir-35-light.css";
 import "Font/lulo-clean-w01-one-bold/lulo-clean-w01-one-bold.css";
 import "./index.css";
+
+import "element-internals-polyfill";
 import "Component/on-intersection-add-class";
 import "Component/progress-bar";
 import "Component/tinted-background";
 import "Component/git";
 import "Component/parallax";
 import "Component/nav";
-import "element-internals-polyfill";
 import "Component/form-control";
 import "Component/on-scroll"
 
