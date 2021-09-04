6789cff3d3bbc4ac9b3122e093af554bca37e080
Brandon Mosher <code.brandon@mosher.xyz>
2021-09-04 11:13:31 -0400

    Broke index.css into multiple files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import "./index.css";
+import "./style/font.css";
+import "./style/structure.css";
+import "./style/backgrounds.css";
+import "./style/scroll.css";
+import "./style/intro.css";
+import "./style/resume.css";
+import "./style/skills.css";
+import "./style/projects.css";
+import "./style/experience.css";
+import "./style/contact-form.css";
 
 import "element-internals-polyfill";
+
 import "Component/on-intersection-set-class";
+import "Component/nav";
 import "Component/progress-bar";
 import "Component/git";
-import "Lib/parallax";
-import "Component/nav";
 import "Component/form-control";
 import "Component/on-scroll/on-scroll.js"
+
+import "Lib/parallax";
 import "Lib/on-scroll"
